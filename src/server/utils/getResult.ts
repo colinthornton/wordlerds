43289cd24cd244317c04d662fb7cdc091f3c0665
Colin Thornton <colinthorntontx@gmail.com>
2023-10-01 15:17:48 +0900

    Fix result calculation

--- src/server/utils/getResult.ts
+++ src/server/utils/getResult.ts
@@ -5,13 +5,23 @@ import {
 } from "~/utils/CharResult";
 
 export function getResult(target: string | string[], attempt: string[]) {
-  return attempt.map((letter, i) => {
-    if (target[i] === letter) {
-      return RESULT_CORRECT;
+  const targetChars =
+    typeof target === "string" ? target.split("") : target.slice();
+  const result = new Array(target.length).fill(RESULT_NOT_FOUND);
+
+  for (let i = 0; i < attempt.length; i++) {
+    const targetIndex = targetChars.findIndex((char) => char === attempt[i]);
+    if (targetIndex === -1) {
+      continue;
     }
-    if (target.includes(letter)) {
-      return RESULT_INCORRECT_PLACE;
+    if (targetIndex === i) {
+      result[i] = RESULT_CORRECT;
+      targetChars[targetIndex] = "";
+    } else {
+      result[i] = RESULT_INCORRECT_PLACE;
+      targetChars[targetIndex] = "";
     }
-    return RESULT_NOT_FOUND;
-  });
+  }
+
+  return result;
 }
