531d5ec279ce01fbe215dbe5eb6edff80eb34552
Colin Thornton <colinthorntontx@gmail.com>
2023-10-08 22:23:35 +0900

    Move links and auth to hamburger menu

--- src/components/AuthStatus.vue
+++ src/components/AuthStatus.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
-const { signIn, signOut, status, session } = useAuth();
+const { signIn, signOut, status } = useAuth();
 </script>
 
 <template>
-  <div class="flex items-center gap-2">
-    <template v-if="status === 'authenticated'">
-      <UButton @click="signOut()" label="Sign Out" size="xs" />
-      <UAvatar :src="session?.user?.avatar" :alt="session?.user?.name" />
-    </template>
-    <UButton
-      v-else
-      :label="status === 'unauthenticated' ? 'Sign In' : ''"
-      size="xs"
-      :loading="status === 'loading'"
-      class="transition-all"
-      @click="signIn('discord')"
-    />
-  </div>
+  <UButton
+    v-if="status === 'authenticated'"
+    @click="signOut()"
+    label="Log Out"
+    size="xs"
+    variant="outline"
+    color="yellow"
+  />
+  <UButton
+    v-else
+    :label="status === 'unauthenticated' ? 'Sign In' : ''"
+    size="xs"
+    :loading="status === 'loading'"
+    class="transition-all"
+    @click="signIn('discord')"
+  />
 </template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,14 +1,65 @@
+<script setup lang="ts">
+const { session } = useAuth();
+
+const pageTitle = computed(() => {
+  const path = useRoute().path;
+  return (
+    {
+      "/": "Co-op Daily",
+      "/coopmugen": "Co-op Mugen",
+    }[path] ?? ""
+  );
+});
+
+const menuOpen = useState(() => false);
+const menuLinks = [
+  {
+    label: "Co-op",
+    links: [
+      { label: "Daily", to: "/" },
+      { label: "Mugen", to: "/coopmugen" },
+    ],
+  },
+];
+</script>
+
 <template>
   <div
     class="min-h-[100svh] overscroll-contain grid grid-cols-1 grid-rows-[auto,1fr]"
   >
     <header class="h-10 border-b border-b-gray-700">
       <UContainer class="flex justify-between items-center h-full px-4 py-2">
-        <AuthStatus />
-        <nav class="flex gap-2">
-          <NuxtLink active-class="text-primary" to="/">Daily</NuxtLink>
-          <NuxtLink active-class="text-primary" to="/coopmugen">Mugen</NuxtLink>
-        </nav>
+        <UAvatar :src="session?.user?.avatar" />
+        <h1 class="uppercase font-bold">{{ pageTitle }}</h1>
+        <UButton
+          icon="i-heroicons-bars-3"
+          color="white"
+          variant="ghost"
+          @click="menuOpen = true"
+        />
+        <USlideover v-model="menuOpen" :ui="{ base: 'p-4 gap-4' }">
+          <div class="flex justify-between">
+            <AuthStatus />
+            <UButton
+              icon="i-heroicons-x-mark"
+              color="white"
+              variant="ghost"
+              @click="menuOpen = false"
+            />
+          </div>
+          <UVerticalNavigation :links="menuLinks">
+            <template #default="{ link }">
+              <div class="relative text-left w-full">
+                <div class="mb-2">{{ link.label }}</div>
+                <UVerticalNavigation
+                  v-if="link.links"
+                  :links="link.links"
+                  :ui="{ active: 'dark:text-primary' }"
+                />
+              </div>
+            </template>
+          </UVerticalNavigation>
+        </USlideover>
       </UContainer>
     </header>
     <slot />
