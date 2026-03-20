5d2c27c46781e597b5e1d05c4187d35f4c284f88
Colin Thornton <colinthorntontx@gmail.com>
2026-03-20 18:01:06 +0900

    Dockerize

--- src/db/dialect.ts
+++ src/db/dialect.ts
@@ -1,7 +1,7 @@
 import { Database } from "bun:sqlite";
 import { BunSqliteDialect } from "kysely-bun-sqlite";
 
-const database = new Database("wordlerds.sqlite");
+const database = new Database("sqlite/wordlerds.sqlite");
 database.run("PRAGMA journal_mode = WAL;");
 
 export const dialect = new BunSqliteDialect({
