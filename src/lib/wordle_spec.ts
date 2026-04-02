baf69214984a37ee3448b49ef375b82f12f7b2ea
Colin Thornton <colinthorntontx@gmail.com>
2026-04-02 19:53:14 +0900

    Wordle game logic

---
+++ src/lib/wordle_spec.ts
@@ -0,0 +1,122 @@
+import { expect, test, describe } from "bun:test";
+import {
+  HardModeError,
+  Wordle,
+  WordleLetter,
+  WordNotInDictionaryError,
+  WordNotInSolutionsError,
+} from "./wordle";
+
+describe("#gameOver", () => {
+  test("false for new game", () => {
+    const game = new Wordle("guess");
+
+    expect(game.gameOver).toBeFalse();
+  });
+
+  test("true for early solved game", () => {
+    const game = new Wordle("guess", ["guess"]);
+
+    expect(game.gameOver).toBeTrue();
+  });
+
+  test("true for late solved game", () => {
+    const game = new Wordle("guess", [
+      "games",
+      "games",
+      "games",
+      "games",
+      "games",
+      "guess",
+    ]);
+
+    expect(game.gameOver).toBeTrue();
+  });
+
+  test("true for max attempts", () => {
+    const game = new Wordle("guess", [
+      "games",
+      "games",
+      "games",
+      "games",
+      "games",
+      "games",
+    ]);
+
+    expect(game.gameOver).toBeTrue();
+  });
+});
+
+describe("results", () => {
+  test("finds solution", () => {
+    const game = new Wordle("guess");
+    game.makeAttempt("guess");
+
+    expect(game.attempts).toEqual([
+      {
+        word: "guess",
+        result: [
+          WordleLetter.Correct,
+          WordleLetter.Correct,
+          WordleLetter.Correct,
+          WordleLetter.Correct,
+          WordleLetter.Correct,
+        ],
+      },
+    ]);
+  });
+
+  test("multiple present letters", () => {
+    const game = new Wordle("droll");
+    game.makeAttempt("llama");
+
+    expect(game.attempts).toEqual([
+      {
+        word: "llama",
+        result: [
+          WordleLetter.Present,
+          WordleLetter.Present,
+          WordleLetter.NotPresent,
+          WordleLetter.NotPresent,
+          WordleLetter.NotPresent,
+        ],
+      },
+    ]);
+  });
+});
+
+describe("errors", () => {
+  test("invalid solution", () => {
+    expect(() => new Wordle("invalid")).toThrow(WordNotInSolutionsError);
+  });
+
+  test("not in dictionary", () => {
+    const game = new Wordle("guess");
+
+    expect(() => game.makeAttempt("lorem")).toThrow(WordNotInDictionaryError);
+  });
+
+  describe("hard mode", () => {
+    test("must play correct letters in same place", () => {
+      const game = new Wordle("hardy", ["hovel"]);
+
+      // missing initial "h"
+      expect(() => game.makeAttempt("there")).toThrow(HardModeError);
+    });
+
+    test("must play present letter", () => {
+      const game = new Wordle("gourd", ["death"]);
+
+      // missing present "d"
+      expect(() => game.makeAttempt("price")).toThrow(HardModeError);
+    });
+
+    test("must play all present letters", () => {
+      const game = new Wordle("llama", ["droll"]);
+
+      // missing second "l"
+      expect(() => game.makeAttempt("lever")).toThrow(HardModeError);
+      expect(() => game.makeAttempt("level")).not.toThrow();
+    });
+  });
+});
