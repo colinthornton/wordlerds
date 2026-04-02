19386031eddc6a8c24831c2485c2a3abd938cd7c
Colin Thornton <colinthorntontx@gmail.com>
2026-04-02 11:17:21 +0900

    Add keyboard component

---
+++ src/views/components/keyboard.ts
@@ -0,0 +1,72 @@
+import { html } from "hono/html";
+
+export const keyboard = (props: {}) =>
+  html`<div
+    class="flex flex-col gap-2 max-w-full w-121 mx-auto touch-manipulation"
+  >
+    ${keys.map(
+      (row) =>
+        html`<div class="flex gap-2 justify-center">
+          ${row.map((key) => button(key))}
+        </div>`,
+    )}
+  </div>`;
+
+const keys = [
+  ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+  ["", "a", "s", "d", "f", "g", "h", "j", "k", "l", ""],
+  ["Enter", "z", "x", "c", "v", "b", "n", "m", "Backspace"],
+];
+
+const button = (key: string) => {
+  switch (key) {
+    case "":
+      return html`<div class="flex-[0.5] -mx-1"></div>`;
+    case "Enter":
+      return html`<button
+        class="btn-icon flex-[1.5] uppercase font-bold h-14 p-0 text-[20px]"
+      >
+        <svg
+          class="size-5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1.5"
+        >
+          <path
+            d="M6 12 3.269 3.126A59.768 59.768 0 0 1 21.485 12 59.77 59.77 0 0 1 3.27 20.876L5.999 12Zm0 0h7.5"
+          />
+        </svg>
+      </button>`;
+    case "Backspace":
+      return html`<button
+        class="btn-icon flex-[1.5] uppercase font-bold h-14 p-0"
+      >
+        <svg
+          class="size-5"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="1.5"
+        >
+          <path
+            d="M12 9.75 14.25 12m0 0 2.25 2.25M14.25 12l2.25-2.25M14.25 12 12 14.25m-2.58 4.92-6.375-6.375a1.125 1.125 0 0 1 0-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 0 1 2.25 2.25v10.5a2.25 2.25 0 0 1-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33Z"
+          />
+        </svg>
+      </button>`;
+    default:
+      return html`<button class="btn flex-1 uppercase font-bold h-14 p-0">
+        ${key}
+      </button>`;
+  }
+};
--- src/views/root.ts
+++ src/views/root.ts
@@ -1,10 +1,10 @@
 import { html } from "hono/html";
 import type { User } from "../db/schema";
 import { layout } from "./layout";
+import { keyboard } from "./components/keyboard";
 
 export const rootView = (props: { user: User }) =>
   layout({
     user: props.user,
-    body: html`<pre><code>${JSON.stringify(props.user, null, 2)}</code></pre>
-      <button id="sign-out">Sign Out</button>`,
+    body: html`${keyboard({})}`,
   });
