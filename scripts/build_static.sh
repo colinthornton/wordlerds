092ce24e400a66ff0267cef1f3415cda08b80726
Colin Thornton <colinthorntontx@gmail.com>
2026-04-04 22:37:05 +0900

    Make build script less ridiculous

--- scripts/build_static.sh
+++
@@ -1,9 +0,0 @@
-set -e
-mkdir -p public/vendor
-cp -r src/public/vendor public
-cp src/public/icon.png public/icon.png
-if [[ -v WATCH ]]; then
-  bun build src/public/wordlerds.ts --outdir public/ --watch
-else
-  bun build src/public/wordlerds.ts --outdir public/
-fi
