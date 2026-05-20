141395f33eff11eacfbd01b6c28ca7144197ad8a
Colin Thornton <colinthorntontx@gmail.com>
2026-05-20 17:48:24 +0900

    srcset for avatar images

--- src/views/components/avatar.ts
+++ src/views/components/avatar.ts
@@ -1,9 +1,14 @@
 import { html } from "hono/html";
 import type { User } from "../../models/user";
 
-export const avatar = (props: { user: User }) =>
-  html`<img
+export const avatar = (props: { user: User }) => {
+  const { name, avatar } = props.user;
+  return html`<img
     class="size-8 shrink-0 object-cover rounded-full"
-    alt="${props.user.name}"
-    src="${props.user.avatar}?size=32"
+    alt="${name}"
+    src="${avatar}?size=32"
+    srcset="${avatar}?size=64 2x, ${avatar}?size=128 4x"
+    width="32"
+    height="32"
   />`;
+};
